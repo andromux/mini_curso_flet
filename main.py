c0a93e807ebc9f55a937f15ede55c4ac273a3d6f
andromux <andromux@proton.me>
2025-06-07 23:41:31 -0700

    BuilModulocal

--- main.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-from routes import setup_routes
-
-def main(page: ft.Page):
-    page.title = "Mini curso de Flet"
-    page.theme_mode = ft.ThemeMode.SYSTEM
-    setup_routes(page)  # Toda la lógica delegada
-
-ft.app(target=main, assets_dir="assets")
